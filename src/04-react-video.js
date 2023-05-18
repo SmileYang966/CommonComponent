82828acb1210883c10f8f96ce54fe0efb158d755
Evan Yang <shancheng.yang@vaillant-group.com>
2023-05-18 10:45:34 +0800

    react-video组件

--- src/04-react-video.js
+++ src/04-react-video.js
@@ -1,12 +1,90 @@
-import { Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View,Dimensions } from 'react-native'
 import React, { Component } from 'react'
+import { WebView } from 'react-native-webview'
+import Video from 'react-native-video'
+import { showModel } from './component/ModelOperations'
+
+
+const VentilationHaloMp4Url = 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'
+const ScreenWidth = Dimensions.get('window').width;
+const ScreenHeight = Dimensions.get('window').height;
+const marginX = 10;
+const marginY = 10;
+
+
+function VideoView(props){
+  return (
+    <View style={{width:ScreenWidth*0.9 , height:ScreenWidth * 0.675, backgroundColor:'white'}}>
+      <Video source={{uri:VentilationHaloMp4Url}}
+        controls={true}
+        fullscreen={true}
+        fullscreenOrientation={'landscape'}
+        ref={(ref)=>{
+          this.player = ref;
+        }}
+        style={styles.videoStyle}   
+      />
+    </View>
+  );
+}
+
 
 export default class ReactVideoScreen extends Component {
+
+  //H5->RN
+  messageInfo(e) {
+    this.setState({
+      title: '隐私政策'
+    });
+  }
+
+  onLoadEnd(e){
+    let data = {
+      version: 'v2.0.0'
+    }
+    this.webView && this.webView.postMessage(JSON.stringify(data));
+  }
+
+  onBuffer(e){
+
+  }
+
+  videoError(error){
+
+  }
+
+  buttonClick(){
+    console.log("buttonClick");
+    showModel(<VideoView/>);
+  }
+
+
   render() {
-    return (
-      <View>
-        <Text>ReactVideoScreen</Text>
+    return ( 
+      <View style={styles.container}>
+        <Pressable onPress={(e) => this.buttonClick()} style={styles.buttonStyle}>
+          <Text>弹出弹框</Text>
+        </Pressable>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  buttonStyle : {
+    width : 150,
+    height : 40,
+    backgroundColor : 'gray',
+    marginTop : 50,
+    alignSelf : 'center',
+    justifyContent:'center',
+    alignItems : 'center'
+  },
+  videoStyle : {
+    width : '100%',
+    height : '100%'
+  }
+})
